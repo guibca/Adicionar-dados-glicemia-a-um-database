d5014fcc5ec57a06b27aa73698b0c8fa6aeee4c0
Guilherme Bueno de Camargo Antonio <124844502+guibca@users.noreply.github.com>
2023-02-13 18:52:48 -0300

    Update querySQL.sql

--- querySQL.sql
+++ querySQL.sql
@@ -1,14 +1,14 @@
 SELECT
 	*,
-    CASE 
+	CASE 
 		WHEN glic_valor >= 150 THEN 'Hiperglicemia'
-        WHEN glic_valor >= 70 THEN 'Normal'
-        ELSE 'Hipoglicemia'
+		WHEN glic_valor >= 70 THEN 'Normal'
+		ELSE 'Hipoglicemia'
 		END AS glic_resultado,
 	CASE
 		WHEN EXTRACT(HOUR FROM glic_data) >= 19 THEN 'Noite'
-        WHEN EXTRACT(HOUR FROM glic_data) >= 13 THEN 'Tarde'
-        WHEN EXTRACT(HOUR FROM glic_data) >= 5 THEN 'Manhã'
-        ELSE 'Noite'
-        END AS hora_dia
+		WHEN EXTRACT(HOUR FROM glic_data) >= 13 THEN 'Tarde'
+		WHEN EXTRACT(HOUR FROM glic_data) >= 5 THEN 'Manhã'
+		ELSE 'Noite'
+		END AS hora_dia
 FROM Glicemia.glic_medicoes
